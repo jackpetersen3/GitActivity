002259fd8c903294d9387043dd5483d24b7c26e0
Jack Petersen <peterja7@os2.engr.oregonstate.edu>
2021-04-15 16:43:22 -0700

    password generator complete

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length):
+    temp = string.ascii_lowercase
+    password = ''.join(random.choice(temp) for i in range (length))
+    print("Randomly generated password is: ", password)
+
+
+generate_password(9)
+generate_password(12)
+generate_password(8)
+generate_password(14)
